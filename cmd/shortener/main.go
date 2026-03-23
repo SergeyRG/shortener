08672d8a2ef57999d663c53fab69f1a73d500d2c
Gilmanov Sergey <gsr87@yandex.ru>
2026-03-23 23:27:42 +0300

    Спринт 2. Итерация 7. №3

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -36,14 +36,14 @@ func run() error {
 
 	rootHandler := logging.WithLogging(handler.RootHandler(svc))
 	redirectHandler := logging.WithLogging(handler.RedirectHandler(svc))
-	JsonShortenHandler := logging.WithLogging((handler.JsonShortenHandler(svc)))
+	JSONShortenHandler := logging.WithLogging((handler.JSONShortenHandler(svc)))
 
 	r := chi.NewRouter()
 	r.Route("/", func(r chi.Router) {
 		r.Post("/", rootHandler)
 		r.Get("/{id}", redirectHandler)
 		r.Get("/{id}/", redirectHandler)
-		r.Post("/api/shorten", JsonShortenHandler)
+		r.Post("/api/shorten", JSONShortenHandler)
 	})
 	logger.Info("запуск приложения")
 	return http.ListenAndServe(cfg.ServerAddress, r)
